ada0ce2ceeb95a40bb7705cb89a1fad2ef892208
firatyedibela <yedibelafirat0@gmail.com>
2025-02-05 18:03:49 +0300

    add handlers module

--- src/app.js
+++ src/app.js
@@ -1,35 +1,21 @@
-import { formValidator } from './modules/form';
 import { calculatorDOM } from './modules/dom';
+import { handlers } from './modules/handlers';
 
 const inputs = document.querySelectorAll('.calculator__input');
+const customTipInput = document.querySelector('.calculator__input--custom');
 const tipOptionButtons = document.querySelectorAll(
   '.calculator__tip-option-btn'
 );
-const customTipInput = document.querySelector('.calculator__input--custom');
 
 inputs.forEach((input) => {
-  input.addEventListener('blur', handleInputChange);
+  input.addEventListener('blur', handlers.handleInputChange);
 });
 
 tipOptionButtons.forEach((option) => {
-  option.addEventListener('click', handleOptionBtnClick);
+  option.addEventListener('click', handlers.handleOptionBtnClick);
 });
 
 customTipInput.addEventListener('click', (e) => {
   calculatorDOM.resetSelectedOption();
   e.target.classList.add('tip-option-selected');
 });
-
-function handleInputChange() {
-  if (formValidator.validate()) {
-    calculatorDOM.renderResults(formValidator.formData);
-  }
-}
-
-function handleOptionBtnClick(e) {
-  calculatorDOM.resetSelectedOption();
-  e.target.classList.add('tip-option-selected');
-  if (formValidator.validate()) {
-    calculatorDOM.renderResults(formValidator.formData);
-  }
-}
---
+++ src/modules/handlers.js
@@ -0,0 +1,23 @@
+import { calculatorDOM } from './dom';
+import { formValidator } from './form';
+
+export const handlers = (function () {
+  function handleInputChange() {
+    if (formValidator.validate()) {
+      calculatorDOM.renderResults(formValidator.formData);
+    }
+  }
+
+  function handleOptionBtnClick(e) {
+    calculatorDOM.resetSelectedOption();
+    e.target.classList.add('tip-option-selected');
+    if (formValidator.validate()) {
+      calculatorDOM.renderResults(formValidator.formData);
+    }
+  }
+
+  return {
+    handleInputChange,
+    handleOptionBtnClick,
+  };
+})();
